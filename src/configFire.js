a6bb57c5908f31871f94d311a8547d76856bd1e1
vero688 <vero.urrutiaa@gmila.coname>
2020-02-25 13:46:03 -0300

    instalaciones de firebase y creacion archivo configuracion de llave de firebas

---
+++ src/configFire.js
@@ -0,0 +1,20 @@
+// TODO: Replace the following with your app's Firebase project configuration
+import  firebase from '../src/App';
+import 'firebase/firebase';
+import  'firebase/database';
+
+firebase.initializeApp ({
+  apiKey: "AIzaSyApeD3RcmaSDiguwNje4koampuoFwm9RII",
+  authDomain: "burqer-queen1.firebaseapp.com",
+  databaseURL: "https://burqer-queen1.firebaseio.com",
+  projectId: "burqer-queen1",
+  storageBucket: "burqer-queen1.appspot.com",
+  messagingSenderId: "524330997269",
+  appId: "1:524330997269:web:1cdfb715a1fcaf5f49601c",
+  measurementId: "G-BVLQSW8VWQ"
+
+});
+
+let db=firebase.firestore();
+
+export default db;
