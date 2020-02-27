ef39505aaf5757eb2ac93781c17cfa46f8056318
vero688 <vero.urrutiaa@gmila.coname>
2020-02-27 18:27:45 -0300

    creando primera vista de la aplicacion inicio con botones para mesero y cheff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 
@@ -7,20 +7,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 class='display-1'>Titulo con boostrap</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1 class='display-1'>Burguer Queen</h1>
+       <div className="bottun" >
+      <button className="bont">
+        <p>Cheeff</p>
+        </button> 
+        <button className="mesero">
+          <p>Mesero </p>
+        </button>
 
-        </a>
+        </div>
+        
       </header>
     </div>
   );
