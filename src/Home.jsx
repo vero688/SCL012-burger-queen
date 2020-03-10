01669d21c81d43b25321904aceed1d0546214f6b
kaarenzu <karenzcortes0211@gmail.com>
2020-03-10 11:06:48 -0300

    Agregamos todos los cambios de cada una y cambiamos el formato de cada archivo

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Home extends Component {
+ render() {
+    return(
+      <div className="Home">
+        <button  type="button" className="btn btn-success btn-lg text-white"> Cocina</button>
+        <button type="button" className="btn btn-danger btn-lg text-white">Mesero</button>
+    </div>
+    
+    );
+  }
+}
+export default Home;
