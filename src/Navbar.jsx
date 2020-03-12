614f7b9bc5e53a10a05fd83b19c876833e8db356
vero688 <vero.urrutiaa@gmila.coname>
2020-03-12 12:42:53 -0300

    resolviendo problemas con archivo de navbar y home

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,13 +8,13 @@ class Navbar extends React.Component{
     render(){
       return <div className="Navbar">
           <div className="container-fluid">    
-  <a  className="Navbar__brand" href="/">
+  <div className="Navbar__brand">
       <img  className="Navbar__brand-logo" src={logo} alt="logo"/>
       <span className="font-weight-light">Burger</span>
       <span className="font-weight-bold">Queen</span>  
       <hr></hr>
       
-  </a>
+  </div>
   <Home/>
   </div>
       </div>;
